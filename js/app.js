b3e36c938e11f63437536d9ea8184eab10c9532e
CodeKasper <miraquienseconecto.0@gmail.com>
2022-06-14 22:07:13 -0300

    update v1.0.8

--- js/app.js
+++ js/app.js
@@ -2,6 +2,9 @@ const resultado = document.querySelector("#resultado");
 const formulario = document.querySelector("#formulario");
 const paginacionDiv = document.querySelector("#paginacion");
 const divHeading = document.querySelector("#heading");
+const alertaDiv = document.querySelector("#alerta");
+
+
 
 const registroPorPagina = 40;
 let totalPaginas;
@@ -49,7 +52,7 @@ function mostrarAlerta(mensaje) {
                 <span class="block sm:inline">${mensaje}</span>
             `;
 
-    formulario.appendChild(alerta);
+    alertaDiv.appendChild(alerta);
 
     setTimeout(() => {
       alerta.remove();
@@ -58,7 +61,6 @@ function mostrarAlerta(mensaje) {
 }
 
 function buscarImagenes() {
- 
   const termino = document.querySelector("#termino").value;
 
   const key = "27765604-3ac229706030bfff75d97cc25";
@@ -82,14 +84,12 @@ function calcularPaginas(total) {
   return parseInt(Math.ceil(total / registroPorPagina));
 }
 function mostrarImagenes(imagenes) {
-    
-  
-  if (imagenes.length === 0) {
+  if (!imagenes.length) {
     mostrarAlerta("No hay resultado de la búsqueda");
   } else {
     while (resultado.firstChild) {
-        resultado.removeChild(resultado.firstChild);
-      }
+      resultado.removeChild(resultado.firstChild);
+    }
     headingResultado();
     // Iterar sobre el arreglo de imagenes y contruir el HTML
     imagenes.forEach((imagen) => {
@@ -98,26 +98,31 @@ function mostrarImagenes(imagenes) {
       resultado.innerHTML += `
 
         
-        <div class="rounded overflow-hidden shadow-lg flex flex-col">
+        <div class="rounded overflow-hidden flex flex-col">
         
         <a data-fslightbox href="${largeImageURL}">
-                    <div class="card-img relative">
+                    <div class="relative">
                     
-                        <img  loading="lazy" class="w-full" data-fslightbox src="${webformatURL}">
+                        <img  loading="lazy" class="w-full h-52" data-fslightbox src="${webformatURL}">
                             
                         <div
                             class="hover:bg-transparent transition duration-300 absolute bottom-0 top-0 right-0 left-0 bg-gray-900 opacity-25">
                         </div>
                     </div>
                     </a>
-                <div class="px-6 py-3 flex flex-row items-center justify-between bg-gray-100">
-                    <span href="#" class="py-1 text-xs font-regular text-gray-900 mr-1 flex flex-row items-center">
-                        <img loading="lazy" class="icon" src="img/views.svg">
-                        <span class="ml-1">${views}</span></span>
-
-                    <span href="#" class="py-1 text-xs font-regular text-gray-900 mr-1 flex flex-row items-center">
-                        <img loading="lazy" class="icon" src="img/like.svg">
-                        <span class="ml-1">${likes}</span></span>
+                    <div class="relative px-6 py-1 mx-auto flex  flex-row items-center justify-between bg-white w-3/4 -mt-4">
+                    <span class="py-1 text-xs font-regular text-gray-900 mr-1 flex flex-row items-center">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 stroke-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
+                  </svg>
+                        <span class="ml-1 font-sans">${views}</span></span>
+            
+                    <span class="py-1 text-xs font-regular text-gray-900 mr-1 flex flex-row items-center">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 stroke-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
+            </svg>
+                        <span class="ml-1 font-sans">${likes}</span></span>
                 </div>
             </div>
         `;
@@ -173,17 +178,20 @@ function headingResultado() {
   const terminoBusqueda = document.querySelector("#termino").value;
   const icon = document.createElement("IMG");
   icon.src = "img/photo.svg";
-  icon.classList.add("title-photo", "mr-2");
+  icon.classList.add("mr-2", "w-8", "h-8");
 
   const heading = document.createElement("H2");
   heading.classList.add(
     "font-semibold",
     "inline-block",
     "border-b-2",
-    "border-indigo-600"
+    "text-rose-800",
+    "uppercase",
+    "border-rose-800"
   );
-  heading.innerHTML = `Imagenes: ${terminoBusqueda}`;
+  heading.innerHTML = `Imágenes: ${terminoBusqueda}`;
 
   divHeading.appendChild(icon);
   divHeading.appendChild(heading);
 }
+
